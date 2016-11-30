ef3e8349f9de26c55e09bb0750404e0adc175a48
Inna Pinsky <inna.pinsky@sap.com>
2016-11-30 12:26:57 +0000

    smart trial app

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("SmartTrialApp.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("SmartTrialApp.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
